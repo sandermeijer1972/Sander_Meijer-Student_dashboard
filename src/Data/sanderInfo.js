30c4834867e047c7d9f1e6dddfce85bcc31dea81
Sander Meijer <sandertje1972@hotmail.com>
2022-01-12 09:54:11 +0100

    changed info

--- src/Data/sanderInfo.js
+++ src/Data/sanderInfo.js
@@ -3,7 +3,7 @@ const sanderInfo = {
     lastname: "Meijer",
     age: 49,
     residence: "Lisse",
-    phone: "06-55122992",
+    phone: "06-55153363",
     email: "sandertje1972@hotmail.com",
     photo: "https://sandermeijer.netlify.app/images/sander.jpg",
     info: "dit zijn de cijfers die ik zelf aan opdrachten heb gegeven, en tellen niet mee in het overzicht van alle studenten"
